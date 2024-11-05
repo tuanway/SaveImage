a316aa0073ffd8524aef6ede4209c9a00f65e824
milanmols <milan.mols@gmail.com>
2024-11-05 16:38:45 +0100

    Update SaveImage.java
    updated to work with new MediaStore API

--- src/android/SaveImage.java
+++ src/android/SaveImage.java
@@ -2,11 +2,10 @@ package com.quiply.cordova.saveimage;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.channels.FileChannel;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -17,22 +16,27 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import android.Manifest;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.os.Build;
 import android.net.Uri;
 import android.os.Environment;
-import android.util.Log;
+
 
 /**
- * The SaveImage class offers a method saving an image to the devices' media gallery.
+ * The SaveImage class offers a method for saving an image to the device's media gallery.
  */
 public class SaveImage extends CordovaPlugin {
     public static final int WRITE_PERM_REQUEST_CODE = 1;
     private final String ACTION = "saveImageToGallery";
+    private final String READ_MEDIA_IMAGES = Manifest.permission.READ_MEDIA_IMAGES;
     private final String WRITE_EXTERNAL_STORAGE = Manifest.permission.WRITE_EXTERNAL_STORAGE;
     private CallbackContext callbackContext;
     private String filePath;
-    
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -53,113 +57,120 @@ public class SaveImage extends CordovaPlugin {
      * args[0] filePath         file path string to image file to be saved to gallery
      */  
     private void saveImageToGallery(JSONArray args, CallbackContext callback) throws JSONException {
-    	this.filePath = args.getString(0);
-    	this.callbackContext = callback;
+        this.filePath = args.getString(0);
+        this.callbackContext = callback;
         Log.d("SaveImage", "SaveImage in filePath: " + filePath);
-        
+
         if (filePath == null || filePath.equals("")) {
-        	callback.error("Missing filePath");
+            callback.error("Missing filePath");
             return;
         }
-        
-        if (PermissionHelper.hasPermission(this, WRITE_EXTERNAL_STORAGE)) {
-        	Log.d("SaveImage", "Permissions already granted, or Android version is lower than 6");
-        	performImageSave();
+
+        // Request permissions based on Android version
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (PermissionHelper.hasPermission(this, READ_MEDIA_IMAGES)) {
+                performImageSave();
+            } else {
+                PermissionHelper.requestPermission(this, WRITE_PERM_REQUEST_CODE, READ_MEDIA_IMAGES);
+            }
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            // Android 10 and 11: No WRITE_EXTERNAL_STORAGE needed, can use MediaStore directly
+            performImageSave();
         } else {
-        	Log.d("SaveImage", "Requesting permissions for WRITE_EXTERNAL_STORAGE");
-        	PermissionHelper.requestPermission(this, WRITE_PERM_REQUEST_CODE, WRITE_EXTERNAL_STORAGE);
-        }      
+            // Android 9 and below: request WRITE_EXTERNAL_STORAGE permission
+            if (PermissionHelper.hasPermission(this, WRITE_EXTERNAL_STORAGE)) {
+                performImageSave();
+            } else {
+                PermissionHelper.requestPermission(this, WRITE_PERM_REQUEST_CODE, WRITE_EXTERNAL_STORAGE);
+            }
+        }
     }
-    
-    
+
     /**
      * Save image to device gallery
      */
-    private void performImageSave() throws JSONException {
-        // create file from passed path
+    private void performImageSave() {
         File srcFile = new File(filePath);
 
-        // destination gallery folder - external storage
-        File dstGalleryFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        if (!srcFile.exists()) {
+            callbackContext.error("Source file does not exist: " + filePath);
+            return;
+        }
 
-        Log.d("SaveImage", "SaveImage dstGalleryFolder: " + dstGalleryFolder);
-
-        try {
-            // Create export file in destination folder (gallery)
-            File expFile = copyFile(srcFile, dstGalleryFolder);
-
-            // Update image gallery
-            scanPhoto(expFile);
-
-            callbackContext.success(expFile.toString());
-        } catch (RuntimeException e) {
-            callbackContext.error("RuntimeException occurred: " + e.getMessage());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            // Use MediaStore for Android 10 and above
+            saveImageToMediaStore(srcFile);
+        } else {
+            // Use traditional file copying for Android 9 and below
+            saveImageToLegacyGallery(srcFile);
         }
     }
 
     /**
-     * Copy a file to a destination folder
-     *
-     * @param srcFile       Source file to be stored in destination folder
-     * @param dstFolder     Destination folder where to store file
-     * @return File         The newly generated file in destination folder
+     * Save image to MediaStore (Android 10+)
      */
-    private File copyFile(File srcFile, File dstFolder) {
-        // if destination folder does not exist, create it
-        if (!dstFolder.exists()) {
-            if (!dstFolder.mkdir()) {
-                throw new RuntimeException("Destination folder does not exist and cannot be created.");
+    private void saveImageToMediaStore(File srcFile) {
+        ContentValues values = new ContentValues();
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmssSSS").format(new Date());
+        values.put(MediaStore.Images.Media.DISPLAY_NAME, "IMG_" + timeStamp + ".jpg");
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+        values.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/YourAppFolder");
+
+        Context context = cordova.getActivity().getApplicationContext();
+        Uri uri = context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+        if (uri != null) {
+            try (FileInputStream inStream = new FileInputStream(srcFile);
+                 OutputStream outStream = context.getContentResolver().openOutputStream(uri)) {
+
+                if (outStream == null) {
+                    callbackContext.error("Error opening output stream for URI");
+                    return;
+                }
+
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = inStream.read(buffer)) > 0) {
+                    outStream.write(buffer, 0, bytesRead);
+                }
+
+                callbackContext.success("Image saved to gallery: " + uri.toString());
+            } catch (IOException e) {
+                callbackContext.error("IOException occurred: " + e.getMessage());
             }
+        } else {
+            callbackContext.error("Failed to save image to gallery.");
         }
+    }
+
+    /**
+     * Save image using traditional file system for Android 9 and below
+     */
+    private void saveImageToLegacyGallery(File srcFile) {
+        File dstFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
 
         // Generate image file name using current date and time
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmssSSS").format(new Date());
         File newFile = new File(dstFolder.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
 
-        // Read and write image files
-        FileChannel inChannel = null;
-        FileChannel outChannel = null;
+        try (FileChannel inChannel = new FileInputStream(srcFile).getChannel();
+             FileChannel outChannel = new FileOutputStream(newFile).getChannel()) {
 
-        try {
-            inChannel = new FileInputStream(srcFile).getChannel();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Source file not found: " + srcFile + ", error: " + e.getMessage());
-        }
-        try {
-            outChannel = new FileOutputStream(newFile).getChannel();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Copy file not found: " + newFile + ", error: " + e.getMessage());
-        }
-
-        try {
             inChannel.transferTo(0, inChannel.size(), outChannel);
-        } catch (IOException e) {
-            throw new RuntimeException("Error transfering file, error: " + e.getMessage());
-        } finally {
-            if (inChannel != null) {
-                try {
-                    inChannel.close();
-                } catch (IOException e) {
-                    Log.d("SaveImage", "Error closing input file channel: " + e.getMessage());
-                    // does not harm, do nothing
-                }
-            }
-            if (outChannel != null) {
-                try {
-                    outChannel.close();
-                } catch (IOException e) {
-                    Log.d("SaveImage", "Error closing output file channel: " + e.getMessage());
-                    // does not harm, do nothing
-                }
-            }
-        }
 
-        return newFile;
+            // Update gallery using legacy scan method
+            scanPhoto(newFile);
+
+            callbackContext.success(newFile.toString());
+        } catch (IOException e) {
+            callbackContext.error("Error saving file: " + e.getMessage());
+        }
     }
 
     /**
-     * Invoke the system's media scanner to add your photo to the Media Provider's database,
-     * making it available in the Android Gallery application and to other apps.
+     * Invoke the system's media scanner to add your photo to the Media Provider's database
+     * for older Android versions.
      *
      * @param imageFile The image file to be scanned by the media scanner
      */
@@ -169,25 +180,21 @@ public class SaveImage extends CordovaPlugin {
         mediaScanIntent.setData(contentUri);
         cordova.getActivity().sendBroadcast(mediaScanIntent);
     }
-    
 
     /**
      * Callback from PermissionHelper.requestPermission method
      */
-	public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
-		for (int r : grantResults) {
-			if (r == PackageManager.PERMISSION_DENIED) {
-				Log.d("SaveImage", "Permission not granted by the user");
-				callbackContext.error("Permissions denied");
-				return;
-			}
-		}
-		
-		switch (requestCode) {
-		case WRITE_PERM_REQUEST_CODE:
-			Log.d("SaveImage", "User granted the permission for WRITE_EXTERNAL_STORAGE");
-			performImageSave();
-			break;
-		}
-	}
+    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
+        for (int r : grantResults) {
+            if (r == PackageManager.PERMISSION_DENIED) {
+                Log.d("SaveImage", "Permission not granted by the user");
+                callbackContext.error("Permissions denied");
+                return;
+            }
+        }
+
+        if (requestCode == WRITE_PERM_REQUEST_CODE) {
+            performImageSave();
+        }
+    }
 }
